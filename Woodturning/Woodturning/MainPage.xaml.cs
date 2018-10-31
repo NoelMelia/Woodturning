9136829e2b00d799b5ae638105468ef6a86130a5
NoelMelia <g00237491@gmit.ie>
2018-10-31 11:36:00 +0000

    Added logo

--- Woodturning/Woodturning/MainPage.xaml.cs
+++ Woodturning/Woodturning/MainPage.xaml.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace Woodturning
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WoodturningApp
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            SetupImageOnThisPage();
+
+
         }
+        private void SetupImageOnThisPage()
+        {
+            var assembly = typeof(MainPage);
+
+            string fileName = "Woodturning.Assets.Images.Newlogo.png";
+
+            newLogo.Source = ImageSource.FromResource(fileName, assembly);
+        }
+
+
     }
 }
