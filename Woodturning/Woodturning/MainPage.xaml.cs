9c4f10f7f35975859cd428a5fb5e7993230a80ab
NoelMelia <g00237491@gmit.ie>
2018-11-09 02:22:12 +0000

    Picker Working Perfectly

--- Woodturning/Woodturning/MainPage.xaml.cs
+++ Woodturning/Woodturning/MainPage.xaml.cs
@@ -4,23 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Woodturning;
-using Login;
-using WoodturningApp;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
+
 namespace WoodturningApp
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
     public partial class MainPage : ContentPage
     {
+        
+        
+
         public MainPage()
         {
             InitializeComponent();
             SetupImageOnThisPage();
-
+            pickerOfItems();
 
         }
+
+        private void pickerOfItems()
+        {
+            MainPicker.ItemsSource = new string[]
+                                            {"Login",
+                                             "Register",
+                                            "Contact Us",
+                                            "Terms",
+                                            "Help"};
+        }
+
         private void SetupImageOnThisPage()
         {
             var assembly = typeof(MainPage);
@@ -28,6 +42,8 @@ namespace WoodturningApp
             string fileName = "Woodturning.Assets.Images.Newlogo.png";
 
             newLogo.Source = ImageSource.FromResource(fileName, assembly);
+
+            //Navigation.PushAsync(new MainPage());
         }
 
         async void bowl_Clicked(object sender, EventArgs e)
@@ -39,5 +55,40 @@ namespace WoodturningApp
         {
             await Navigation.PushAsync(new BiroPage());
         }
+
+
+        private void MainPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            
+            switch (picker.SelectedIndex)
+            {
+                case 0:
+                    Navigation.PushAsync(new Login());
+                    break;
+                case 1:
+                    Navigation.PushAsync(new Register());
+                    break;
+                case 2:
+                    //Navigation.PushAsync(new Login());
+                    break;
+                case 3:
+                    //Navigation.PushAsync(new Register());
+                    break;
+                case 4:
+                    //Navigation.PushAsync(new Login());
+                    break;
+                
+                default:
+                    break;
+
+
+
+            }
+
+        }
+
+
+
     }
 }
