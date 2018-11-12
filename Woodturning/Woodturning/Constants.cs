cb264d38d6e8b7a445b165f9b07cab4c446abd63
NoelMelia <g00237491@gmit.ie>
2018-11-12 11:19:22 +0000

    Login and Register page partly working

---
+++ Woodturning/Woodturning/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Woodturning
+{
+    class Constants
+    {
+        public static string Username = "Xamarin";
+        public static string Password = "password";
+    }
+}
--- Woodturning/Woodturning/Register.xaml.cs
+++ Woodturning/Woodturning/Register.xaml.cs
@@ -1,20 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Woodturning
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Register : ContentPage
 	{
-		public Register ()
-		{
-			InitializeComponent ();
-		}
-	}
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSignUpButtonClicked(object sender, EventArgs e)
+        {
+            var user = new User()
+            {
+                Username = usernameEntry.Text,
+                Password = passwordEntry.Text,
+                Email = emailEntry.Text
+            };
+
+            // Sign up logic goes here
+
+            var signUpSucceeded = AreDetailsValid(user);
+            if (signUpSucceeded)
+            {
+                var rootPage = Navigation.NavigationStack.FirstOrDefault();
+                if (rootPage != null)
+                {
+                    App.IsUserLoggedIn = true;
+                    Navigation.InsertPageBefore(new MainPage(), Navigation.NavigationStack.First());
+                    await Navigation.PopToRootAsync();
+                }
+            }
+            else
+            {
+                messageLabel.Text = "Sign up failed";
+            }
+        }
+
+        bool AreDetailsValid(User user)
+        {
+            return (!string.IsNullOrWhiteSpace(user.Username) && !string.IsNullOrWhiteSpace(user.Password) && !string.IsNullOrWhiteSpace(user.Email) && user.Email.Contains("@"));
+        }
+    }
 }
